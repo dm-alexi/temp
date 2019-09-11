e31cbc0b78638b766444a5871a363f556d4dc9e3
Sharice Scarecrow <sscarecr@ox-e1.21-school.ru>
2019-09-11 18:56:42 +0300

    normalized

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sscarecr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 15:54:58 by sscarecr          #+#    #+#             */
-/*   Updated: 2019/07/17 16:14:48 by sscarecr         ###   ########.fr       */
+/*   Updated: 2019/09/11 18:55:52 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,57 +15,56 @@
 #include "get_next_line.h"
 #include "libft.h"
 
-typedef struct s_buf
+int		getbuf(char **line, t_buf *buf, size_t *slen)
 {
-    size_t len;
-    char str[BUF_SIZE];
-} t_buf;
+	char	*s;
+	size_t	n;
+	int		finished;
 
-static int getbuf(char **line, t_buf *buf, size_t *slen)
-{
-    char *s;
-    size_t n;
-    int finished = 0;
-
-    if (!buf->len)
-        return 0;
-    s = buf->str;
-    while (s - buf->str < buf->len && *s != '\n')
-        ++s;
-    n = s - buf->str;
-    if (!(n + 1) || !(*line = (char*)realloc(*line, *slen + n + 1)))
-        return (-1);
-    ft_memcpy(*line + *slen, buf->str, n);
-    *slen += n;
-    *(*line + *slen) = '\0';
-    if (n < buf->len)
-        finished = (++n == buf->len);
-    ft_memcpy(buf->str, buf->str + n, buf->len - n);
-    buf->len -= n;
-    return (finished || buf->len);
+	if (!buf->len)
+		return (0);
+	finished = 0;
+	s = buf->str;
+	while ((size_t)(s - buf->str) < buf->len && *s != '\n')
+		++s;
+	n = s - buf->str;
+	if (!(n + 1) || !(*line = (char*)realloc(*line, *slen + n + 1)))
+		return (-1);
+	ft_memcpy(*line + *slen, buf->str, n);
+	*slen += n;
+	*(*line + *slen) = '\0';
+	if (n < buf->len)
+		finished = (++n == buf->len);
+	ft_memcpy(buf->str, buf->str + n, buf->len - n);
+	buf->len -= n;
+	return (finished || buf->len);
 }
 
-int get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-    static t_buf buf = {0};
-    size_t slen;
-    int r;
+	static t_buf	buf = {0, {0}};
+	size_t			slen;
+	int				r;
 
-    *line = NULL;
-    slen = 0;
-    if (!buf.len)
-    {
-        if ((r = read(fd, &(buf.str), BUF_SIZE)) <= 0)
-            return (r);
-        buf.len = r;
-    }
-    while (!(r = getbuf(line, &buf, &slen)))
-        if (!(buf.len = read(fd, &(buf.str), BUF_SIZE)))
-            break;
-    if (r == -1 || buf.len == -1)
-    {
-        free(*line);
-        return (-1);
-    }
-    return (1);
+	*line = NULL;
+	slen = 0;
+	if (!buf.len)
+	{
+		if ((r = read(fd, &(buf.str), BUF_SIZE)) <= 0)
+			return (r);
+		buf.len = r;
+	}
+	while (!(r = getbuf(line, &buf, &slen)))
+	{
+		r = read(fd, &(buf.str), BUF_SIZE);
+		if (r <= 0)
+			break ;
+		buf.len = r;
+	}
+	if (r == -1)
+	{
+		free(*line);
+		return (-1);
+	}
+	return (1);
 }
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,14 +6,20 @@
 /*   By: sscarecr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 15:54:58 by sscarecr          #+#    #+#             */
-/*   Updated: 2019/07/17 16:14:48 by sscarecr         ###   ########.fr       */
+/*   Updated: 2019/09/11 18:46:29 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 # define BUF_SIZE 2
-int get_next_line(const int fd, char **line);
+
+typedef struct	s_buf
+{
+	size_t	len;
+	char	str[BUF_SIZE];
+}				t_buf;
+
+int				get_next_line(const int fd, char **line);
 
 #endif

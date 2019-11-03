d09363b8ec756d6467c0b49d810ef0f27b18162c
Sharice Scarecrow <sscarecr@oa-e5.21-school.ru>
2019-11-03 16:36:06 +0300

    pushswap: libft updated

--- pushswap/libft/ft_printf/ft_printf_n.c
+++ pushswap/libft/ft_printf/ft_printf_n.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_printf_n.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/05 22:08:24 by sscarecr          #+#    #+#             */
-/*   Updated: 2019/11/02 17:23:43 by sscarecr         ###   ########.fr       */
+/*   Created: 2019/10/23 23:42:58 by sscarecr          #+#    #+#             */
+/*   Updated: 2019/10/26 14:41:52 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <stdarg.h>
 
-char	*ft_strsub(char const *s, unsigned int start, size_t len)
+int		ft_printf_n(va_list *va, int n)
 {
-	char	*str;
-	char	*t;
+	int		*p;
 
-	if (!s || !(len + 1) || !(str = (char*)malloc(len + 1)))
-		return (NULL);
-	t = str;
-	s += start;
-	while ((size_t)(t - str) < len)
-		*t++ = *s++;
-	*t = '\0';
-	return (str);
+	p = ((int*)va_arg(*va, int*));
+	if (p)
+		*p = n;
+	return (0);
 }

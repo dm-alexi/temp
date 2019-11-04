204a49ce3052cb2b8ba9ff874edca0b07e5511b1
dm-alexi <dm2.alexi@gmail.com>
2019-11-05 00:22:02 +0300

    pushswap started

---
+++ pushswap/pushswap.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include "pushswap.h"
+
+void	show_stack(t_node *a)
+{
+	printf("%d\n", a->num);
+	for (t_node *t = a->d; t != a; t = t->d)
+		printf("%d\n", t->num);
+}
+
+int 	main(int ac, char **av)
+{
+	int			n;
+	int			fd;
+	t_flags		flags;
+	t_node		*a;
+	t_node		*b;
+
+	if (ac > 1 && (n = get_flags(ac, av, &flags)) > 0)
+	{
+		a = get_args(n, av + ac - n);
+		show_stack(a);
+        if ((fd = (flags.filename ? open(flags.filename, O_RDONLY) : 0)) < 0)
+			file_error(flags.filename);
+		b = NULL;
+
+        clear_lists(a, b);
+	}
+    return 0;
+}

e3ffa9dfe73216509c8ed65b9b3cd1a0f6bdbe64
Sharice Scarecrow <sscarecr@il-g3.21-school.ru>
2019-11-13 21:14:26 +0300

    generate functional

--- psnew/generate.c
+++ psnew/generate.c
@@ -1,17 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   generate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/13 20:44:00 by sscarecr          #+#    #+#             */
+/*   Updated: 2019/11/13 21:13:18 by sscarecr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <time.h>
-#include <conio.h>
 #include "libft/libft.h"
 
-int		usage(void)
+static int		usage(void)
 {
-	ft_printf("Usage: generate <number> [-filename]\n");
+	ft_printf("usage: generate <number> [target_file]\n");
 	return (0);
 }
 
-void	shuffle(int *arr, int n)
+static void		error(const char *s)
+{
+	write(2, "Error: ", 7);
+	write(2, s, ft_strlen(s));
+	write(2, "\n", 1);
+	exit(1);
+}
+
+static void		shuffle(int *arr, int n)
 {
 	int		i;
 	int		r;
@@ -29,26 +48,30 @@ void	shuffle(int *arr, int n)
 	}
 }
 
-int		main(int ac, char **av)
+int				main(int ac, char **av)
 {
     int		fd;
     int		n;
     int		i;
     int		*arr;
 
-    if (ac < 2 || ac > 4)
+	if (ac < 2 || ac > 3)
 		return (usage());
-	if ((n = ft_atoi(av[1])) <= 0 || !(arr = (int*)malloc(sizeof(int * n))))
-		error();
+	if ((n = ft_atoi(av[1])) <= 0)
+		error("positive number required.");
+	if (!(arr = (int*)malloc(sizeof(int) * n)))
+		error("memory allocation failed.");
 	i = -1;
 	while (++i < n)
 		arr[i] = i + 1;
 	shuffle(arr, n);
-	if ((fd = (open(ac == 3 ? av[2] : "arrnum", O_WRONLY | O_CREAT))) < 0)
-		error();
+	if ((fd = (open((ac == 3 ? av[2] : "arrnum"),
+		O_CREAT | O_WRONLY | O_TRUNC, 0666))) < 0)
+		error("file open error.");
 	i = 0;
 	while (i < n)
 		ft_dprintf(fd, "%d ", arr[i++]);
+	free(arr);
 	close(fd);
-    return 0;
+    return (0);
 }
--- psnew/pushswap.c
+++ psnew/pushswap.c
@@ -6,7 +6,7 @@
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 21:46:02 by sscarecr          #+#    #+#             */
-/*   Updated: 2019/11/12 22:17:27 by sscarecr         ###   ########.fr       */
+/*   Updated: 2019/11/13 21:12:39 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,10 @@ int		main(int ac, char **av)
 		t.a = get_args(n, av + ac - n, &n);
 		t.a_count = n;
 		if ((fd = (flags.filename ?
-		open(flags.filename, O_WRONLY | O_CREAT) : 1)) < 0)
+		open(flags.filename, O_CREAT | O_WRONLY | O_TRUNC, 0666) : 1)) < 0)
 			file_error(flags.filename);
 		sort(&t);
-		print_log(&t);
-		show_stacks(&t);
+		//show_stacks(&t);
 		print_result(&t, fd);
 		clear_stacks(&t);
 		if (fd > 0)

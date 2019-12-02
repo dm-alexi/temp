8c3bb1322f06e22e2c27756b6156b4720c512512
dm-alexi <dm2.alexi@gmail.com>
2019-12-03 01:42:39 +0300

    d01 started

---
+++ d01/ex02/oddeven.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+echo "Enter a number: ";
+while (($f = fgets(STDIN)) !== false)
+{
+        $f = trim($f);
+        if (!is_numeric($f))
+                echo "'".$f."' is not a number\n";
+        else if ($f % 2)
+             echo "The number ".$f." is odd\n";
+        else
+                echo "The number ".$f." is even\n";
+        echo "Enter a number: ";
+}
+echo "\n";
+?>

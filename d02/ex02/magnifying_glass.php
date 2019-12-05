22a499f8fec12d9800c920850d39732e523afd4e
Sharice Scarecrow <sscarecr@ox-u2.21-school.ru>
2019-12-05 06:40:26 +0300

    final fixes

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -4,9 +4,9 @@ if ($argc < 2 || !($f = file_get_contents($argv[1])))
 	exit;
 $f = preg_replace_callback("/<a (.*?)\/a>/s", function($m)
 {
-	$m[1] = preg_replace_callback("/ title=([\"'])(.*?)\\1/s", function($m)
+	$m[1] = preg_replace_callback("/title=([\"'])(.*?)\\1/s", function($m)
 	{
-		return " title=".$m[1].strtoupper($m[2]).$m[1];
+		return "title=".$m[1].strtoupper($m[2]).$m[1];
 	}, $m[1]);
 	$m[1] = preg_replace_callback("/(>(.*?)<)/s", function($m)
 	{
--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -6,21 +6,21 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $str = curl_exec($ch);
 curl_close($ch);
-$dir = substr(strstr($argv[1], "//"), 2);
+$dir = strstr($argv[1], "//") !== false ? substr(strstr($argv[1], "//"), 2) : $argv[1];
 if (!(file_exists($dir)))
 	mkdir($dir);
 preg_match_all("/<img.*?>/", $str, $arr);
 foreach ($arr[0] as $val)
 {
-	preg_match("/src=\"(.*?)\"/", $val, $tmp);
-	$link = preg_match("/^http/", $tmp[1]) ? $tmp[1] : $argv[1].$tmp[1];
+	preg_match("/src=([\"'])(.*?)\\1/", $val, $tmp);
+	$link = preg_match("/^http/", $tmp[2]) ? $tmp[2] : $argv[1].$tmp[2];
 	if (!($ch = curl_init($link)))
 		continue;
 	$filename = $dir.strrchr($link, "/");
 	$f = fopen($filename, "w");
 	curl_setopt($ch, CURLOPT_FILE, $f);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5000);
 	$success = curl_exec($ch);
 	fclose($f);
 	curl_close($ch);

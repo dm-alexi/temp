f5a641548a05964893443b7105a3dbe93c54884d
Sharice Scarecrow <sscarecr@ox-u2.21-school.ru>
2019-12-06 13:05:48 +0300

    ex03 fixed

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,8 +1,8 @@
 <?php
 if ($_GET["action"] === "set")
-	setcookie($_GET["name"], $_GET["value"], time() + 60 * 60 * 24 * 30);
+	setcookie($_GET["name"], $_GET["value"], time() + 2592000);
 else if ($_GET["action"] === "get")
-	echo $_COOKIE[$_GET["name"]];
+	echo ($_COOKIE[$_GET["name"]] !== NULL ? $_COOKIE[$_GET["name"]]."\n" : "");
 else if ($_GET["action"] === "del")
-	setcookie($_GET["name"], 0, time());
+	setcookie($_GET["name"], NULL);
 ?>

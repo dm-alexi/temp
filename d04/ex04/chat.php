2c5e5e30b4d5ebbccea61760fe8d307790ae29d5
Sharice Scarecrow <sscarecr@ox-u2.21-school.ru>
2019-12-07 13:36:47 +0300

    chat fixed

--- d04/ex04/chat.php
+++ d04/ex04/chat.php
@@ -2,7 +2,10 @@
 $filename = "../private/chat";
 if (file_exists($filename))
 	date_default_timezone_set("Europe/Moscow");
-	$arr = unserialize(file_get_contents($filename));
-	foreach ($arr as $val)
-		echo date("[H:i]", $val["time"])." <b>".$val["login"]."</b>: ".$val["msg"]."<br />", "\n";
+	if (file_exists($filename))
+	{
+		if ($arr = unserialize(file_get_contents($filename)))
+			foreach ($arr as $val)
+				echo date("[H:i]", $val["time"])." <b>".$val["login"]."</b>: ".$val["msg"]."<br />", "\n";
+	}
 ?>

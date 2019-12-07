0726135ada4acf137aeb112ff7ac5e691b135c21
Sharice Scarecrow <sscarecr@ox-a5.21-school.ru>
2019-12-07 13:05:39 +0300

    d04ex03 fixed

--- d04/ex03/login.php
+++ d04/ex03/login.php
@@ -6,10 +6,14 @@ if ($_GET["login"] !== NULL && $_GET["login"] !== "" && $_GET["passwd"] !== NULL
 {
 	$_SESSION["loggued_on_user"] = $_GET["login"];
 	echo "OK\n";
+	exit;
 }
-else
+if ($_POST["login"] !== NULL && $_POST["login"] !== "" && $_POST["passwd"] !== NULL && $_POST["passwd"] !== "" && auth($_POST["login"], $_POST["passwd"]))
 {
-	$_SESSION["loggued_on_user"] = "";
-	echo "ERROR\n";
+	$_SESSION["loggued_on_user"] = $_POST["login"];
+	echo "OK\n";
+	exit;
 }
+$_SESSION["loggued_on_user"] = "";
+echo "ERROR\n";
 ?>

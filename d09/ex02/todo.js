3ef6a47e2c8f5116ab1241927ac81f2c47b4d821
Sharice Scarecrow <sscarecr@ox-u2.21-school.ru>
2019-12-13 22:02:39 +0300

    d03ex02

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,29 +1,30 @@
 function setCookie(value)
 {
-    var list = document.getElementById("ft_list");
     var expire = new Date();
     expire.setMonth(expire.getMonth() + 1);
     document.cookie = "name=" + value + ";expires=" + expire.toGMTString();
-    alert("content=" + value + ";expires=" + expire.toGMTString());
-    alert(document.cookie);
+}
+
+function getCookie(cookie_name) 
+{
+    var res = document.cookie.match('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
+    if (res)
+        return (unescape(res[2]));
+    else
+        return null;
 }
 
 function savelist() 
 {
-    var listObject = {};
-    var str = '';
+    var arr = [];
     var list = document.getElementById("ft_list");
-    for (var count = 0; count < list.children.length; count++) {
-        listObject.pos = count;
-        listObject.val = list.children[count].textContent;
-        str += JSON.stringify(listObject);
-    }
-    setCookie(str);
+    for (var i = 0; i < list.children.length; ++i)
+        arr[i] = list.children[i].textContent;
+    setCookie(JSON.stringify(arr));
 }
 
 function delEntry()
 {
-    var list = document.getElementById("ft_list");
     if (confirm("Are you sure you want to delete: \"" + this.textContent.substr(2) + "\"?"))
         this.remove();
     savelist();
@@ -42,7 +43,15 @@ function addEntry()
         savelist();
     }
 }
-
-var list = document.getElementById("ft_list");
-//console.log(list.cookie);
-alert(document.cookie);
+window.onload = function()
+{
+    var list = document.getElementById("ft_list");
+    var lst = JSON.parse(getCookie("name"));
+    for (var i = 0; i < lst.length; ++i)
+    {
+        var elem = document.createElement("div");
+        elem.onclick = delEntry;
+        elem.appendChild(document.createTextNode(lst[i]));
+        list.append(elem);
+    }
+}

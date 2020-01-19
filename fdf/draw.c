9e78adc0e87e8c9927e525bd134e1eab6d3f7466
Sharice Scarecrow <sscarecr@ox-c1.21-school.ru>
2020-01-19 20:45:52 +0300

    gradient added, not attached to z yet

--- fdf/draw.c
+++ fdf/draw.c
@@ -6,7 +6,7 @@
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 19:44:17 by sscarecr          #+#    #+#             */
-/*   Updated: 2020/01/19 18:01:10 by sscarecr         ###   ########.fr       */
+/*   Updated: 2020/01/19 19:56:50 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,37 @@ void	image_clear(t_image *image)
 		image->s[i] = 0;
 }
 */
+int red (int color)
+{
+    return (color >> 16);
+}
+
+int green(int color)
+{
+    return (color >> 8) & 0xFF;
+}
+
+int blue(int color)
+{
+    return (color & 0xFF);
+}
+
+int		gradient(t_vertex *a, t_vertex *b, int steps, int i)
+{
+	int		stepR;
+	int		stepG;
+	int		stepB;
+	int		color;
+	double	k;
+
+	k = (double)i / steps;
+	stepR = red(a->color) + (red(b->color) - red(a->color))*k;
+	stepG = green(a->color) + (green(b->color) - green(a->color))*k;
+	stepB = blue(a->color) + (blue(b->color) - red(a->color))*k;
+	color = (stepR << 16) + (stepG << 8) + stepB;
+	return (color);
+}
+
 void	image_put_pixel(t_image *image, int x, int y, int color)
 {
 	int		i;
@@ -57,7 +88,8 @@ void	image_draw_line(t_image *image, t_vertex *a, t_vertex *b)
 		step = (double)(a->y - b->y) / (a->x - b->x);
 		while (i <= b->x - a->x)
 		{
-			image_put_pixel(image, a->x + i, a->y + i * step, 0xffffff - (a->z + b->z) * 1000);
+			//image_put_pixel(image, a->x + i, a->y + i * step, 0xffffff - (a->z + b->z) * 1000);
+			image_put_pixel(image, a->x + i, a->y + i * step, gradient(a, b, a->x - b->x, i));
 			++i;
 		}
 	}
@@ -66,7 +98,8 @@ void	image_draw_line(t_image *image, t_vertex *a, t_vertex *b)
 		step = (double)(a->x - b->x) / (a->y - b->y);
 		while (i <= b->y - a->y)
 		{
-			image_put_pixel(image, a->x + i * step, a->y + i, 0xffffff - (a->z + b->z) * 1000);
+			//image_put_pixel(image, a->x + i * step, a->y + i, 0xffffff - (a->z + b->z) * 1000);
+			image_put_pixel(image, a->x + i * step, a->y + i, gradient(a, b, a->y - b->y, i));
 			++i;
 		}
 	}

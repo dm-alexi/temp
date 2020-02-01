d09c9a41e3cf89a1c352bb1db8ab393e2a886fc8
Shinohara Tristimun <stristim@il-i3.21-school.ru>
2020-02-01 21:16:28 +0300

    fdf: menu ver 2

--- fdf/menu.c
+++ fdf/menu.c
@@ -6,23 +6,12 @@
 /*   By: stristim <stristim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 19:35:03 by stristim          #+#    #+#             */
-/*   Updated: 2020/02/01 20:52:02 by stristim         ###   ########.fr       */
+/*   Updated: 2020/02/01 21:15:41 by stristim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include "fdf.h"
-#define WHITE 0xFFFFFF
-# define MIDCOLOR 0xFFFFFF
-# define BLUE 0x0000FF
-# define RED 0xFF0000
-# define GREEN 0x00FF00
-# define VIOLET 0xFF00FF
-# define YELLOW 0xFFFF00
-# define CYAN 0x00FFFF
-# define PINK 0xFF0080
-# define ORANGE 0xFF8000
-# define AVOCADO 0x00FF80
 
 void ft_menu(t_map *map)
 {
@@ -38,19 +27,19 @@ void ft_menu(t_map *map)
     mlx_string_put(map->mlx, map->win, 15, 245, WHITE, "High   |  Low");
     mlx_string_put(map->mlx, map->win, 15, 270, RED, "RED");
     mlx_string_put(map->mlx, map->win, 15, 290, BLUE, "BLUE");
-    mlx_string_put(map->mlx, map->win, 15, 310, CYAN, "CYAN");
-    mlx_string_put(map->mlx, map->win, 15, 330, PINK, "PINK");
-    mlx_string_put(map->mlx, map->win, 15, 350, YELLOW, "YELLOW");
-    mlx_string_put(map->mlx, map->win, 15, 370, LIGHT_BLUE, "CYAN");
+    mlx_string_put(map->mlx, map->win, 15, 310, PINK, "PINK");
+    mlx_string_put(map->mlx, map->win, 15, 330, CYAN, "CYAN");
+    mlx_string_put(map->mlx, map->win, 15, 350, GREEN, "GREEN");
+    mlx_string_put(map->mlx, map->win, 15, 370, VIOLET, "VIOLET");
     mlx_string_put(map->mlx, map->win, 15, 390, PINK, "PINK");
     mlx_string_put(map->mlx, map->win, 15, 410, ORANGE, "ORANGE");
     mlx_string_put(map->mlx, map->win, 15, 430, AVOCADO, "AVOCADO");
-    mlx_string_put(map->mlx, map->win, 110, 270, BLUE, "BLUE");
-    mlx_string_put(map->mlx, map->win, 110, 290, RED, "RED");
-    mlx_string_put(map->mlx, map->win, 110, 310, GREEN, "GREEN");
-    mlx_string_put(map->mlx, map->win, 110, 330, VIOLET, "VIOLET");
-    mlx_string_put(map->mlx, map->win, 110, 350, YELLOW, "YELLOW");
-    mlx_string_put(map->mlx, map->win, 110, 370, LIGHT_BLUE, "CYAN");
+    mlx_string_put(map->mlx, map->win, 110, 270, RED, "RED");
+    mlx_string_put(map->mlx, map->win, 110, 290, BLUE, "BLUE");
+    mlx_string_put(map->mlx, map->win, 110, 310, PINK, "PINK");
+    mlx_string_put(map->mlx, map->win, 110, 330, CYAN, "CYAN");
+    mlx_string_put(map->mlx, map->win, 110, 350, GREEN, "GREEN");
+    mlx_string_put(map->mlx, map->win, 110, 370, VIOLET, "VIOLET");
     mlx_string_put(map->mlx, map->win, 110, 390, PINK, "PINK");
     mlx_string_put(map->mlx, map->win, 110, 410, ORANGE, "ORANGE");
     mlx_string_put(map->mlx, map->win, 110, 430, AVOCADO, "AVOCADO");

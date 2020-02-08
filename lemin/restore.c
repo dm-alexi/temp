ccb45c001e562e12fbdea9a181b3d31bafe474d1
Sharice Scarecrow <sscarecr@il-j4.21-school.ru>
2020-02-08 19:52:02 +0300

    libft: restoration started

---
+++ lemin/restore.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   restore.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/08 19:38:52 by sscarecr          #+#    #+#             */
+/*   Updated: 2020/02/08 19:51:22 by sscarecr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lemin.h"
+
+void			graph_restore(t_graph *graph)
+{
+	t_node *t;
+
+	t = graph->finish;
+	
+}
+
+static void		node_restore(t_graph *graph, t_node *out, t_node *in)
+{
+
+}

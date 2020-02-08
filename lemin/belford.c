ae814ca474e50ef9649f34da45c0cd8678f114fb
Sharice Scarecrow <sscarecr@il-j4.21-school.ru>
2020-02-08 18:42:39 +0300

    lemin: bellman-ford implemented; second path found

--- lemin/belford.c
+++ lemin/belford.c
@@ -6,14 +6,13 @@
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 16:30:09 by sscarecr          #+#    #+#             */
-/*   Updated: 2020/02/08 17:23:14 by sscarecr         ###   ########.fr       */
+/*   Updated: 2020/02/08 18:39:27 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
 #include "lemin.h"
 
-void		reset_distance(t_graph *graph)
+void	reset_distance(t_graph *graph)
 {
 	int		i;
 
@@ -22,25 +21,32 @@ void		reset_distance(t_graph *graph)
 		graph->nodes[i++]->distance = -1;
 	graph->start->distance = 0;
 }
-/*
-void			belford(t_graph *graph)
+
+void	belford(t_graph *graph)
 {
 	int		i;
 	int		j;
 	int		fin;
 	t_edge	*t;
-	
+
 	reset_distance(graph);
-	i = 0;
-	while (i < graph->node_num - 1)
+	i = graph->node_num - 1;
+	fin = 0;
+	while (i-- && !fin)
 	{
 		j = -1;
 		fin = 1;
 		while (++j < graph->node_num && (t = graph->nodes[j]->edges))
 			while (t)
 			{
-				if ()
+				if (graph->nodes[j]->distance != -1 && (t->node->distance == -1
+				|| t->node->distance > graph->nodes[j]->distance + t->len))
+				{
+					t->node->distance = graph->nodes[j]->distance + t->len;
+					t->node->prev = graph->nodes[j];
+					fin = 0;
+				}
+				t = t->next;
 			}
-
 	}
-}*/
+}
--- lemin/main.c
+++ lemin/main.c
@@ -6,7 +6,7 @@
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/27 21:27:08 by sscarecr          #+#    #+#             */
-/*   Updated: 2020/02/08 17:20:49 by sscarecr         ###   ########.fr       */
+/*   Updated: 2020/02/08 18:41:55 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@ int		main(void)
 	t_edge		*path2;
 
 	graph = get_graph();
-	//print_graph(graph);
 	bfs(graph);
 	print_graph(graph);
 	print_path(path = get_path(graph));
 	path_split(graph, path);
 	print_graph(graph);
-	reset_distance(graph);
-	bfs(graph);
+	belford(graph);
 	print_graph(graph);
+	print_path(path);
 	print_path(path2 = get_path(graph));
 	delete_path(path);
 	delete_graph(graph);

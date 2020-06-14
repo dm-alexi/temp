bdbdd7c5949513db2f1a34059ecc37e88fad962b
dm-alexi <dm2.alexi@gmail.com>
2020-06-14 17:00:37 +0300

    reduce champion

--- corewar/BuggyCrutch.s
+++ corewar/BuggyCrutch.s
@@ -26,20 +26,5 @@ entry:
 	sti		r1, %7, %1
 	live	%21
 	fork	%:entry
-	sti		r1, %7, %1
-	live	%21
-	fork	%:entry
-	sti		r1, %7, %1
-	live	%21
-	fork	%:entry
-	sti		r1, %7, %1
-	live	%21
-	fork	%:entry
-	sti		r1, %7, %1
-	live	%21
-	fork	%:entry
-	sti		r1, %7, %1
-	live	%21
-	fork	%:entry
 	and		r1, %0, r2
 	zjmp	%:entry

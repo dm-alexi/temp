5c183d8f22c527b6ea029443c2c7caec3667771d
dm-alexi <dm2.alexi@gmail.com>
2020-08-09 01:53:07 +0300

    libft: gnl updated

--- libft/include/get_next_line.h
+++ libft/include/get_next_line.h
@@ -6,26 +6,28 @@
 /*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 15:54:58 by sscarecr          #+#    #+#             */
-/*   Updated: 2020/07/31 22:11:01 by sscarecr         ###   ########.fr       */
+/*   Updated: 2020/08/09 00:54:02 by sscarecr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 1024
+# define ARRAY_SIZE 256
 
 typedef struct	s_buf
 {
 	int				len;
-	char			*offset;
+	char			*start;
 	struct s_buf	*next;
 	char			str[BUFF_SIZE];
 }				t_buf;
 
 typedef struct	s_file
 {
-	int		len;
-	t_buf	**arr;
+	int				fd;
+	t_buf			*buffer;
+	struct s_file	*next;
 }				t_file;
 
 #endif

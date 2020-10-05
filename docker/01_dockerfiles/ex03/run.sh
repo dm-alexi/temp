8adb88cc3f278dbd0880001719f96c9117b96619
dm-alexi <dm2.alexi@gmail.com>
2020-10-05 17:57:13 +0300

    docker upd ex03

--- docker/01_dockerfiles/ex03/run.sh
+++ docker/01_dockerfiles/ex03/run.sh
@@ -1,1 +1,6 @@
-docker run -it --rm --name gitlab -p 8080:80 -p 8022:22 -p 8443:443 --privileged ft-gitlab
+docker run -it --rm --name gitlab -p 8080:80 -p 8022:22 -p 8443:443 \
+	-e DOCKER_IP="172.17.0.1" \
+	--shm-size=4g --privileged ft-gitlab
+
+#-e DOCKER_IP=$(echo $DOCKER_HOST | cut -d/ -f3 | cut -d: -f1) \
+# git config --global http.sslverify false

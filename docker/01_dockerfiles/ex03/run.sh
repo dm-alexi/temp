58ac3d35f868747c77a2b0a62ea8c45aa51e6790
dm-alexi <dm2.alexi@gmail.com>
2020-10-06 12:13:44 +0300

    docker finally got ex03 right

--- docker/01_dockerfiles/ex03/run.sh
+++ docker/01_dockerfiles/ex03/run.sh
@@ -1,6 +1,1 @@
-docker run -it --rm --name gitlab -p 8080:80 -p 8022:22 -p 8443:443 \
-	-e DOCKER_IP="172.17.0.1" \
-	--shm-size=4g --privileged ft-gitlab
-
-#-e DOCKER_IP=$(echo $DOCKER_HOST | cut -d/ -f3 | cut -d: -f1) \
-# git config --global http.sslverify false
+docker run -it --rm --name gitlab --shm-size=4g --privileged ft-gitlab
